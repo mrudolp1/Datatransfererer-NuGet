1455a0785ebc428bca8b26966efd0ca5da8a5359
Aidan Walsh <Aidan.Walsh@crowncastle.com>
2022-08-05 10:06:18 -0400

    Modified Structure class with report options

--- DataTransferer/Objects/Structure/Structure.vb
+++ DataTransferer/Objects/Structure/Structure.vb
@@ -609,7 +609,8 @@ Partial Public Class EDSStructure
     Public Property DrilledPiers As New List(Of DrilledPier)
     Public Property GuyAnchorBlocks As New List(Of GuyedAnchorBlock)
 
-    Public Property reportOptions As ReportOptions
+    Public Property ReportOptions As ReportOptions
+    Public Property SiteInfo As SiteInfo
 
     'The structure class should return itself if the parent is requested
     Private _ParentStructure As EDSStructure
@@ -818,7 +819,7 @@ Partial Public Class EDSStructure
         'Next
     End Sub
 #End Region
-
+    
 #Region "Check Changes"
     Public Function CompareEDS(other As EDSObject, Optional ByRef changes As List(Of AnalysisChange) = Nothing) As Boolean
 
